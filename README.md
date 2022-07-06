b52e05b85422a39a92f96a8c60550e9d01eb89ab
Dani <s.leslie94@gmail.com>
2022-07-06 14:45:00 -0400

    refractor code; update html and css; remove images

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Sign-Up Form
 
-A simple sign-up form. Utilizes validation with a focus on accessibility. 
+A simple sign-up form that tests validation. 
+
+# About
+
+This form will confirm
+
+- a value for first and last name
+- a valid email address
+- a phone number (optional)
+- a password that must follow specific criteria
+- a password confirmation that matches the previous password
+
+before submission
+
+# Features
+
+- Display of error messages connected to password validation
+
+# Links
+
+- [Assignment page](https://www.theodinproject.com/lessons/node-path-intermediate-html-and-css-sign-up-form) "The Odin Project sign-up form assignment"
 
--- scripts.js
+++ scripts.js
@@ -18,68 +18,64 @@ password.addEventListener('focus', function(){
     message.style.display = 'block';
 });
 
-
-
 password.addEventListener('keyup', validationPopup);
 
 function passwordValidate(){
-    if (password.value != confirmPassword.value){
+    if (confirmPassword.value != password.value){
       confirmPassword.setCustomValidity('Passwords do not match!');
     } else{
       return   
     }
 }
 
-// password validation
-
 function validationPopup(){
-  // Validate lowercase letters
+  // validate lowercase letters
   let lowerCaseLetters = /[a-z]/g;
   if(password.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
+    letter.classList.remove('invalid');
+    letter.classList.add('valid');
   } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
+    letter.classList.remove('valid');
+    letter.classList.add('invalid');
 }
 
-  // Validate capital letters
+  // validate capital letters
   let upperCaseLetters = /[A-Z]/g;
   if(password.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
+    capital.classList.remove('invalid');
+    capital.classList.add('valid');
   } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
+    capital.classList.remove('valid');
+    capital.classList.add('invalid');
   }
 
-  // Validate numbers
+  // validate numbers
   let numbers = /[0-9]/g;
   if(password.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
+    number.classList.remove('invalid');
+    number.classList.add('valid');
   } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
+    number.classList.remove('valid');
+    number.classList.add('invalid');
   }
 
   // validate special characters
   let specialCharacters = /[$@&+\-\/#_?!]/g;
   if(password.value.match(specialCharacters)) {
-      character.classList.remove("invalid");
-      character.classList.add("valid");
+      character.classList.remove('invalid');
+      character.classList.add('valid');
     } else {
-      character.classList.remove("valid");
-      character.classList.add("invalid");
+      character.classList.remove('valid');
+      character.classList.add('invalid');
     }
 
-  // Validate length
+  // validate length
   if(password.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
+    length.classList.remove('invalid');
+    length.classList.add('valid');
   } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
+    length.classList.remove('valid');
+    length.classList.add('invalid');
   }
 }
 
